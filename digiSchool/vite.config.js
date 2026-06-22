88f8d1dccef656b9547f95f101f4c62076b40cbe
Rooney Odhiambo <rooneykabuor2004@gmail.com>
2026-06-22 07:55:36 +0300

    fix(deploy): change vite base path to root to prevent 404s on vercel

--- digiSchool/vite.config.js
+++ digiSchool/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DigiShule/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 2000,
   }
