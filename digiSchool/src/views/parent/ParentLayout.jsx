eb60acfa9f26ceb30b65ef3e3f405433eb2e54f3
Rooney Odhiambo <rooneykabuor2004@gmail.com>
2026-07-06 19:29:32 +0300

    Fix Parent Portal white screen crash

--- digiSchool/src/views/parent/ParentLayout.jsx
+++ digiSchool/src/views/parent/ParentLayout.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useOutletContext } from 'react-router-dom';
 
 export default function ParentLayout() {
+  const context = useOutletContext();
+  
   return (
     <div className="parent-layout">
       {/* 
         The PortalLayout provides the sidebar and header. 
         This is just a local wrapper for the parent specific routes.
       */}
-      <Outlet />
+      <Outlet context={context} />
     </div>
   );
 }
